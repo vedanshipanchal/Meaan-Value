6a3f88048e81db83fbf61a7cc4abc2eb2fc13f7e
vedanshipanchal <vedanshipanchal@gmail.com>
2024-09-21 18:04:00 +0100

    Mean Value

---
+++ main.py
@@ -0,0 +1,13 @@
+mean1 = 10
+wrongnum = 23
+correctnum =7
+total = 87
+#sum
+sum = mean1*total
+print("the added val is:")
+#correct sum val
+num2 = sum - ((wrongnum)-(correctnum))
+print("New number :",num2)
+#correct avg
+mean2 = num2/total
+print("correct mean:", mean2)
